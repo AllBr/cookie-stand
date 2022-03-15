d69e71e340d4765392e51127c9aa049a4052d9bf
Allen Brazier <allenbrazier@gmail.com>
2022-03-14 23:57:43 -0700

    Update app.js
    random math function and html content

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-//
+// Lab 06 JavaScript file.
+
+// random number gen, same as used in class
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// connects to HTML to fil-in content
+let cityProfile = document.getElementById('content');
